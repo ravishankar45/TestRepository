85d771ee3720d3f2ff4ebce07e2cfe4b23fb0bdb
Ravi Shankar <ravi.shankar@citiustech.com>
2019-02-04 16:07:21 +0530

    First Test Commit

---
+++ FreeCRMSiteAutomation/src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,14 @@
+package com.qa.util;
+
+import java.io.FileNotFoundException;
+
+import com.qa.Base.Base_Class;
+
+public class TestUtil  {
+
+	
+
+	
+	public static long PAGE_LOAD_TIMEOUT=30;
+	public static long IMPLICIT_TIMEOUT=20;
+}
